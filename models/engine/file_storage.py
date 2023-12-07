7a11c22fb4dbe07e806863e16bad8a952450cf4e
janenyasoro <janenyasoro@gmail.com>
2023-12-07 04:08:09 -0800

    A class BaseModel that defines all common attributes/methods for other classes:

---
+++ models/engine/file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This defines the FileStorage class."""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
