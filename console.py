7050d912e752cefbd77d938da57977faf722c510
janenyasoro <janenyasoro@gmail.com>
2023-12-08 05:11:19 -0800

    Updating the command interpreter (console.py) to have these commands: show,create and destroy

--- console.py
+++ console.py
@@ -29,6 +29,20 @@ class HBNBCommand(cmd.Cmd):
         """End of File signal to exit the program."""
         print("")
         return True
+    
+    def do_create(self,arg):
+        """Used to create <class>
+        Create a new class instance and print its id.
+        """
+        arg1 = parse(arg)
+        if len(arg1) == 0:
+            print("** class name missing **")
+        elif arg1[0] not in HNBNCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg1[0])().id)
+            storage.save()
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
 
