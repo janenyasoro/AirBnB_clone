36cc72031c7e17f3a1fffc7e8a02cb55045cb864
janenyasoro <janenyasoro@gmail.com>
2023-12-10 12:06:31 -0800

    Making corrections

--- models/base_model.py
+++ models/base_model.py
@@ -44,6 +44,7 @@ class BaseModel:
         rdict["updated_at"] = self.updated_at.isoformat()
         rdict["_class_"] = self._class_._name_
         return rdict
+
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
