2eb22f5667c7456d3d60c679e7ae4d47c7e49b13
janenyasoro <janenyasoro@gmail.com>
2023-12-11 01:09:05 -0800

    Doing tests for the attributes

--- tests/test_save_reload_base_model.py
+++ tests/test_save_reload_base_model.py
@@ -2,6 +2,7 @@
 from models import storage
 from models.base_model import BaseModel
 
+def test_save_reload_base_model():
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
--- tests/test_save_reload_user.py
+++ tests/test_save_reload_user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.user import User
 
 all_objs = storage.all()
+def test_save_reload_user():
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
